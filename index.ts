cf5e26dbcf697c7195650135c4b871e1c85bbffd
Christian Payne <christian.p@modelmatch.com>
2021-09-17 22:48:12 -0700

    Added commands and got rid of broadcaster check.

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
+// Import the types from comfy.js package.
 import {
   ComfyJSInstance
 } from "comfy.js";
 
+// Import the instance of the bot.
 const ComfyJS : ComfyJSInstance = require('comfy.js');
+
+// Set what channel you want to connect to.
 const twitchChannel = 'chrisgriffin522';
 
-
+// Set the onCommand function to be called when a command is sent.
 ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
-  if( flags.broadcaster && command === "test" ) {
+  if( command === "test" ) {
     console.log( "!test was typed in chat" );
   }
 }
 
+// Start the bot.
 ComfyJS.Init( twitchChannel );
