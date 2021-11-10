7b69a9608fe8e0790ff79a5ed159abc5484b3b0b
Christian Payne <christianpayne522@gmail.com>
2021-11-10 14:27:36 -0800

    Added chat message console logs.

--- index.ts
+++ index.ts
@@ -9,6 +9,11 @@ const ComfyJS : ComfyJSInstance = require('comfy.js');
 // Set what channel you want to connect to.
 const twitchChannel = 'chrisgriffin522';
 
+ComfyJS.onChat = ( user, message, flags, self ) => {
+  if ( self ) return;
+  console.log( `${user}: ${message}` );
+}
+
 // Set the onCommand function to be called when a command is sent.
 ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
   if( command === "test" ) {
